6a481f10914d35b8ae4bb10484aab847a535a6e2
Carlos Alejandro Gutierrez Sandoval <carlos.gutierrez@gluwa.com>
2022-01-11 21:44:58 -0600

    exit if min_peers is not sustained

--- validator/sawtooth_validator/server/core.py
+++ validator/sawtooth_validator/server/core.py
@@ -452,6 +452,9 @@ class Validator:
         else:
             self._start()
 
+    class RebootException(Exception):
+        ...
+
     def _start(self):
         self._consensus_dispatcher.start()
         self._consensus_service.start()
@@ -465,12 +468,16 @@ class Validator:
 
         self._completer.set_on_batch_received(self._incoming_batch_sender.send)
         signal_event = threading.Event()
+        force_reset = threading.Event()
+        self._gossip._topology._exit = force_reset
 
         signal.signal(signal.SIGTERM,
                       lambda sig, fr: signal_event.set())
         # This is where the main thread will be during the bulk of the
         # validator's life.
-        while not signal_event.is_set():
+        while not signal_event.is_set() or force_reset.is_set():
+            if force_reset.is_set():
+                raise self.RebootException()
             signal_event.wait(timeout=20)
 
     def stop(self):
